aacaea538e9b05fef6ccadc420fb7878a3b53493
klebao123 <klebersonsantos1998@gmail.com>
2020-03-12 21:43:01 -0300

    commit

---
+++ trabalho-2020.03.03/exemplo-while.php
@@ -0,0 +1,12 @@
+<?php
+
+$controle = 0;
+$tabuada = 9;
+
+while( $controle <= 10 ){
+    $resultado = $tabuada * $controle;
+    echo $tabuada . 'x' . $controle.
+    echo "<br>";
+    $controle = $controle + 1;
+}
+?>
